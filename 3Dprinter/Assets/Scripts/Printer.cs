59b2f31c8ae6774d940a067a3c5fc3798bbd56b3
Timon van den Brink <Timon van den Brink>
2017-04-03 14:27:14 +0200

    Added initial printer

---
+++ 3Dprinter/Assets/Scripts/Printer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Printer : MonoBehaviour {
+
+    public bool Busy;
+
+    public bool AbsoluteCoordinates;
+
+    public Vector3 positionHead;
+    public Vector3 positionTable;
+    public float positionExtruder;
+
+    public bool isBusy() {
+        return Busy;
+    }
+
+    public void setCoordinatesRelative(bool relative = true) {
+
+    }
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
