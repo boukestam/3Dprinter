27f740d6d4f2d1fc23cc630653c675ffd47fd44f
scottmackay2 <scottmackay@live.nl>
2017-04-03 14:25:31 +0200

    Added beginning of gcode reading. Needed push to sync with teammate

---
+++ 3Dprinter/Assets/Gcodes.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gcodes {
+
+    public const float INVALID_NUMBER = -10000000000;
+
+    public const string MOVE0 = "G0";
+    public const string MOVE1 = "G1";
+    public const string HOME_AXIS = "G28";
+    public const string SET_ABOSLUTE = "G90";
+    public const string SET_CURRENT_POS = "G92";
+
+    public const string FAN_ON = "M106";
+    public const string FAN_OFF = "M107";
+    public const string SET_EXTRUDER_TEMP = "M104";
+    public const string SET_BED_TEMP = "M140";
+
+    public const string SET = "S";
+    public static Tuple<string, float> X = new Tuple<string, float>("X", Gcodes.INVALID_NUMBER);
+    public static Tuple<string, float> Y = new Tuple<string, float>("Y", Gcodes.INVALID_NUMBER);
+    public static Tuple<string, float> Z = new Tuple<string, float>("Z", Gcodes.INVALID_NUMBER);
+    public static Tuple<string, float> E = new Tuple<string, float>("E", Gcodes.INVALID_NUMBER);
+    public static Tuple<string, float> F = new Tuple<string, float>("F", Gcodes.INVALID_NUMBER);
+    public static Tuple<string, float> S = new Tuple<string, float>("S", Gcodes.INVALID_NUMBER);
+
+}
---
+++ 3Dprinter/Assets/Tuple.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tuple<T, U> {
+    public T Key { get; set; }
+    public U Value { get; set; }
+
+    public Tuple(T key, U value) {
+        Key = key;
+        Value = value;
+    }
+}
+
+public static class Tuple {
+    public static Tuple<T, U> Create<T, U>(T key, U value) {
+        return new Tuple<T, U>(key, value);
+    }
+}
