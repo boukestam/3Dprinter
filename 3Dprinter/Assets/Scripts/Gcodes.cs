6d51e6d76cf2cd381d528c51abfb01e7be283091
scottmackay2 <scottmackay@live.nl>
2017-04-06 12:37:08 +0200

    Merge

--- 3Dprinter/Assets/Scripts/Gcodes.cs
+++ 3Dprinter/Assets/Scripts/Gcodes.cs
@@ -18,21 +18,4 @@ public class Gcodes {
     public const int FAN_OFF = 107;
     public const int SET_EXTRUDER_TEMP = 104;
     public const int SET_BED_TEMP = 140;
-    /*public static readonly Tuple<char, int> MOVE1 = new Tuple<char, int>('G', 1);
-    public static readonly Tuple<char, int> HOME_AXIS = new Tuple<char, int>('G', 28);
-    public static readonly Tuple<char, int> SET_ABOSLUTE = new Tuple<char, int>('G', 90);
-    public static readonly Tuple<char, int> SET_CURRENT_POS = new Tuple<char, int>('G', 92);
-
-    public static readonly Tuple<char, int> FAN_ON = new Tuple<char, int>('M', 106);
-    public static readonly Tuple<char, int> FAN_OFF = new Tuple<char, int>('M', 107);
-    public static readonly Tuple<char, int> SET_EXTRUDER_TEMP = new Tuple<char, int>('M', 104);
-    public static readonly Tuple<char, int> SET_BED_TEMP = new Tuple<char, int>('M', 140);*/
-    /*
-    public static WriteTuple<char, float> X = new WriteTuple<char, float>('X', Gcodes.INVALID_NUMBER);
-    public static WriteTuple<char, float> Y = new WriteTuple<char, float>('Y', Gcodes.INVALID_NUMBER);
-    public static WriteTuple<char, float> Z = new WriteTuple<char, float>('Z', Gcodes.INVALID_NUMBER);
-    public static WriteTuple<char, float> E = new WriteTuple<char, float>('E', Gcodes.INVALID_NUMBER);
-    public static WriteTuple<char, float> F = new WriteTuple<char, float>('F', Gcodes.INVALID_NUMBER);
-    public static WriteTuple<char, float> S = new WriteTuple<char, float>('S', Gcodes.INVALID_NUMBER);
-    */
 }
